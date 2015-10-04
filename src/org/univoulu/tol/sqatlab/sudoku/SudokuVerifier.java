d13e62154bf03ff938c8974727efc475bc4f46c7
somename <someemail>
2015-10-04 19:38:15 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -15,15 +15,15 @@ public class SudokuVerifier{
 				}
 				}
 			}
-		}
-		public static Boolean LengthVerifier(String s){
+		
+		public static boolean LengthVerifier(String s){
 			if(s.length()!=81){
 				System.out.println("Lenth doesn't equal to 81 characters!");
 				return false;
 			}
 			return true;
 		}
-		public static Boolean NumVerifier(String s){
+		public static boolean NumVerifier(String s){
 		 for(int i=0;i<s.length();i++){
 			 int n=s.charAt(i);
 			 if(n<49||n>57){
@@ -33,7 +33,29 @@ public class SudokuVerifier{
 		 }
 		 return true;
 		}
-		public static Boolean RowVerifier(int m []){
+		public static boolean Verifier(int m[] ) {
+			// TODO Auto-generated method stub
+			if(!RowVerifier(m)){
+				return false;
+			}
+			if(!ColumnVerifier(m)){
+				return false;
+			}
+			if(!SmallSudokuVerifier(m)){
+				return false;
+			}
+			return true;
+		}
+		public static int[] StrIntoArray(String s) {
+			// TODO Auto-generated method stub
+			int m[]=new int [81];
+			for(int i=0;i<81;i++){
+				int n=s.charAt(i);
+				m[i]=n-48;
+			}
+			return m;
+		}
+		public static boolean RowVerifier(int m []){
 			int n[][]=new int [9][9];
 			for(int i=0;i<9;i++){
 				for(int j=0;j<9;j++){
@@ -45,7 +67,8 @@ public class SudokuVerifier{
 			}
 			return true;
 		}
-		public static Boolean ColumnVerifier(int m []){
+
+		public static boolean ColumnVerifier(int m[]){
 			int n[][]=new int [9][9];
 			for(int i=0;i<9;i++){
 				for(int j=0;j<9;j++){
@@ -57,4 +80,14 @@ public class SudokuVerifier{
 			}
 			return true;
 		}
+
+		public static boolean SmallSudokuVerifier(int m [] ) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		public static boolean ArrayVerifier(int[] is) {
+			// TODO Auto-generated method stub
+			return false;
+		}
 		
+}

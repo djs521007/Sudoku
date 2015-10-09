62df1b204048642e072e9a90a438ac72deacfc16
Liu Lei <djs521007@gmail.com>
2015-10-09 22:03:24 +0300

    Fix bugs

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -15,17 +15,19 @@ public class TestSudokuVerifier {
 	@Test
 	public void testUnique() {
 		SudokuVerifier sudo = new SudokuVerifier();
-		
 		assertEquals(false,sudo.NumVerifier("41a369825632158947958724316825437169791586432346912758289643571573291684164875293"));
-		
 	}	
-	
 //	@Test
 //	public void testGird() {
 //		SudokuVerifier sudo = new SudokuVerifier();
 //		assertEquals(-2,sudo.Verifier(sudo.StrIntoArray("I can't find a sutiable sudoku to test this one")));
 //	}	
+	@Test
+	public void testRow() {
+		SudokuVerifier sudo = new SudokuVerifier();
 	
+		assertEquals(-3,sudo.Verifier(sudo.StrIntoArray("447369825632158947958724316825437169791586432346912758289643571573291684164875293")));		
+	}
 	@Test
 	public void testColum() {
 		SudokuVerifier sudo = new SudokuVerifier();	
@@ -33,11 +35,4 @@ public class TestSudokuVerifier {
 
 				
 	}	
-	
-	@Test
-	public void testRow() {
-		SudokuVerifier sudo = new SudokuVerifier();
-	
-		assertEquals(-3,sudo.Verifier(sudo.StrIntoArray("447369825632158947958724316825437169791586432346912758289643571573291684164875293")));		
-	}
 }
